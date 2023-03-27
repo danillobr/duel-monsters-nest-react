c75d4b4ce1c888b7ce9cbf9c07e6a908132b3061
danillobr <danillorodriguesabreu@gmail.com>
2023-03-26 22:47:35 -0300

    finalizing database connection fix and migrations

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,16 @@
-FROM node:18-alpine
+FROM node:lts-alpine
 
 WORKDIR /usr/app
 
 COPY package.json .
 COPY package-lock.json .
 
-RUN npm install
+# RUN npm install
+RUN npm i --f
 
 COPY . .
 
 EXPOSE 3333
 
 CMD npm run start:dev
+# CMD ["npm","run","start:dev"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { UserModule } from './accounts/user.module';
+import { typeOrmConfigAsync } from './config/typeorm.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      database: process.env.DB_DATABASE,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      synchronize: true,
-      entities: [__dirname + '/**/*.entity{.js,.ts}'],
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
     UserModule,
   ],
   controllers: [AppController],
