5515ce51d1f7548792cd57d6a54da4f87367ff38
danillobr <danillorodriguesabreu@gmail.com>
2023-08-22 21:57:42 -0300

    Log Monitoring using Winston and Logtail

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { typeOrmConfig } from './configs/typeorm.config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import { LoggerInterceptor } from './interceptors/logger.interceptor';
-import { WinstonModule } from 'nest-winston';
-import { winstonConfig } from './configs/winston.config';
+// import { LoggerInterceptor } from './interceptors/logger.interceptor';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { mailerConfig } from './configs/mailer.config';
 import { dataSourceOptions } from './db/data-source-cli';
@@ -15,17 +12,16 @@ import 'dotenv/config';
 @Module({
   imports: [
     TypeOrmModule.forRoot(dataSourceOptions),
-    WinstonModule.forRoot(winstonConfig),
     MailerModule.forRoot(mailerConfig),
     UsersModule,
     AuthModule,
   ],
   controllers: [],
-  providers: [
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: LoggerInterceptor,
-    },
-  ],
+  // providers: [
+  //   {
+  //     provide: APP_INTERCEPTOR,
+  //     useClass: LoggerInterceptor,
+  //   },
+  // ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,35 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { useContainer } from 'class-validator';
-import { WinstonModule } from 'nest-winston';
-import { winstonConfig } from './configs/winston.config';
 import 'dotenv/config';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
+const { Logtail } = require('@logtail/node');
+const { LogtailTransport } = require('@logtail/winston');
+import * as winston from 'winston';
+
+// const logtail = new Logtail(process.env.TOKEN_LOGTAIL);
 
 async function bootstrap() {
-  const logger = WinstonModule.createLogger(winstonConfig);
-  const app = await NestFactory.create(AppModule, { logger });
-
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     transform: true,
+  // let app = await NestFactory.create(AppModule, {
+  //   logger: WinstonModule.createLogger({
+  //     transports: [new LogtailTransport(logtail)],
   //   }),
-  // );
+  // });
 
-  // useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  // const logger = WinstonModule.createLogger(winstonConfig);
+  // const app = await NestFactory.create(AppModule, { logger });
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(3333);
 }
 
